41d9cc8bb210a28cf2c0335619d5506afc2dc900
Tiago <tiagonegri237@gmail.com>
2018-05-23 16:18:00 -0300

    class that manages an certain base

--- BaseBehavior/Source/Base.h
+++ BaseBehavior/Source/Base.h
@@ -1,12 +1,17 @@
 #include "WorkerList.h"
+#include "BuildingManager.h"
+#include "BWAPI.h"
 
 #define max_distance 400
+
+using namespace BWAPI;
 
 class Base{
     private:
         WorkerList *workers;
         Position basePosition;
         BuildingManager *buildingList;
+		Requisition *baseRequisitions;
 
         int distance(Position a,Position b);
 
