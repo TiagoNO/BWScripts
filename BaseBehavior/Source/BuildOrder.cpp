38d06974dd1e9d796512ef7f9752b64fd2d4dd2a
Tiago <tiagonegri237@gmail.com>
2018-06-04 15:48:29 -0300

    class that retrieves the units to build next

---
+++ BaseBehavior/Source/BuildOrder.cpp
@@ -0,0 +1,63 @@
+#include "BuildOrder.h"
+
+void ZergBuildOrder::checkMilitaryRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition,max_distance).end(); i++){
+
+	}
+}
+
+void ZergBuildOrder::checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+
+	}
+}
+
+void ZergBuildOrder::checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+
+	}
+}
+
+
+
+
+
+void TerranBuildOrder::checkMilitaryRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+
+	}
+}
+
+void TerranBuildOrder::checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+
+	}
+}
+
+void TerranBuildOrder::checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+
+	}
+}
+
+
+
+
+
+void ProtossBuildOrder::checkMilitaryRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+		
+	}
+}
+
+void ProtossBuildOrder::checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+
+	}
+}
+
+void ProtossBuildOrder::checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+
+	}
+}
