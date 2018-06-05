c24baa3133aa62805b67fb434e42559f04079b09
TiagoNO <tiagonegri237@gmail.com>
2018-06-05 02:56:59 -0300

    Simple build order implemented

--- BaseBehavior/Source/Base/BuildOrder.cpp
+++ BaseBehavior/Source/Base/BuildOrder.cpp
@@ -1,40 +1,19 @@
 #include "BuildOrder.h"
 
-void ZergBuildOrder::checkMilitaryRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+
+void ZergBuildOrder::checkMilitaryRequisitions(set<UnitType> *requisitions, Position basePosition) {
 	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition,max_distance).end(); i++){
 
 	}
 }
 
-void ZergBuildOrder::checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+void ZergBuildOrder::checkResourceRequisitions(set<UnitType> *requisitions, Position basePosition) {
 	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
 
 	}
 }
 
-void ZergBuildOrder::checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
-	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
-
-	}
-}
-
-
-
-
-
-void TerranBuildOrder::checkMilitaryRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
-	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
-
-	}
-}
-
-void TerranBuildOrder::checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
-	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
-
-	}
-}
-
-void TerranBuildOrder::checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+void ZergBuildOrder::checkSpecialRequisitions(set<UnitType> *requisitions, Position basePosition) {
 	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
 
 	}
@@ -44,20 +23,52 @@ void TerranBuildOrder::checkSpecialRequisitions(set<UnitTypes> *requisitions, Po
 
 
 
-void ProtossBuildOrder::checkMilitaryRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
-	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
-		
-	}
-}
-
-void ProtossBuildOrder::checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+void TerranBuildOrder::checkMilitaryRequisitions(set<UnitType> *requisitions, Position basePosition) {
 	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
 
 	}
 }
 
-void ProtossBuildOrder::checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition) {
+void TerranBuildOrder::checkResourceRequisitions(set<UnitType> *requisitions, Position basePosition) {
 	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
 
 	}
+}
+
+void TerranBuildOrder::checkSpecialRequisitions(set<UnitType> *requisitions, Position basePosition) {
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+	}
+}
+
+
+
+
+
+void ProtossBuildOrder::checkMilitaryRequisitions(set<UnitType> *requisitions, Position basePosition) {
+	int P_Gateway = 0;
+	int P_Cybernetics_Core = 0;
+	for (std::set<Unit *>::const_iterator i = Broodwar->getUnitsInRadius(basePosition, max_distance).begin(); i != Broodwar->getUnitsInRadius(basePosition, max_distance).end(); i++) {
+		if((*i)->getType() == UnitTypes::Protoss_Gateway){
+			P_Gateway ++;
+		}
+		if((*i)->getType() == UnitTypes::Protoss_Cybernetics_Core){
+			P_Cybernetics_Core++;
+		}
+	}
+	if(P_Gateway < Protoss_Gateway){
+		requisitions->insert(UnitTypes::Protoss_Gateway);
+	}
+	if(P_Cybernetics_Core == 0 && P_Gateway >= 1){
+		requisitions->insert(UnitTypes::Protoss_Cybernetics_Core);	
+	}
+}
+
+void ProtossBuildOrder::checkResourceRequisitions(set<UnitType> *requisitions, Position basePosition) {
+	if(Broodwar->self()->supplyUsed() - 5*Broodwar->elapsedTime()/180 < Broodwar->self()->supplyTotal()){
+		requisitions->insert(UnitTypes::Protoss_Pylon);
+	}
+}
+
+void ProtossBuildOrder::checkSpecialRequisitions(set<UnitType> *requisitions, Position basePosition) {
+
 }
--- BaseBehavior/Source/Base/BuildOrder.h
+++ BaseBehavior/Source/Base/BuildOrder.h
@@ -1,36 +1,42 @@
 #pragma once
 #include <set>
 #include "BWAPI.h"
-
+#include "BWAPI.h"
 #define max_distance 400
 
 using namespace std;
+using namespace BWAPI;
+
+enum Max_Unit_Number{
+	Protoss_Gateway = 5,
+	Protoss_Cybernetics_Core = 1,
+};
 
 class BuildOrder {
 	public:
-		virtual void checkMilitaryRequisitions(set<UnitTypes> *requisitions,Position basePosition);
-		virtual void checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition);
-		virtual void checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition);
+		virtual void checkMilitaryRequisitions(set<UnitType> *requisitions,Position basePosition);
+		virtual void checkResourceRequisitions(set<UnitType> *requisitions, Position basePosition);
+		virtual void checkSpecialRequisitions(set<UnitType> *requisitions, Position basePosition);
 };
 
 
 class ZergBuildOrder : public BuildOrder {
 	public:
-		void checkMilitaryRequisitions(set<UnitTypes> *requisitions, Position basePosition);
-		void checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition);
-		void checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition);
+		void checkMilitaryRequisitions(set<UnitType> *requisitions, Position basePosition);
+		void checkResourceRequisitions(set<UnitType> *requisitions, Position basePosition);
+		void checkSpecialRequisitions(set<UnitType> *requisitions, Position basePosition);
 };
 
 class TerranBuildOrder : public BuildOrder {
 	public:
-		void checkMilitaryRequisitions(set<UnitTypes> *requisitions, Position basePosition);
-		void checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition);
-		void checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition);
+		void checkMilitaryRequisitions(set<UnitType> *requisitions, Position basePosition);
+		void checkResourceRequisitions(set<UnitType> *requisitions, Position basePosition);
+		void checkSpecialRequisitions(set<UnitType> *requisitions, Position basePosition);
 };
 
 class ProtossBuildOrder : public BuildOrder {
 	public:
-		void checkMilitaryRequisitions(set<UnitTypes> *requisitions, Position basePosition);
-		void checkResourceRequisitions(set<UnitTypes> *requisitions, Position basePosition);
-		void checkSpecialRequisitions(set<UnitTypes> *requisitions, Position basePosition);
+		void checkMilitaryRequisitions(set<UnitType> *requisitions, Position basePosition);
+		void checkResourceRequisitions(set<UnitType> *requisitions, Position basePosition);
+		void checkSpecialRequisitions(set<UnitType> *requisitions, Position basePosition);
 };
